a31d9bdd0dc809110b64266b1732d901b2be48ac
holman <pinedaholmandavid12@gmail.com>
2025-11-29 15:03:27 -0500

    fibonacci

--- Ejercicios.java
+++ Ejercicios.java
@@ -352,7 +352,16 @@ public class Ejercicios {
      */
     public int fibonacci(int n) {
         // TODO: Implementar
-        return 0;
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+
+        int a = 0, b = 1;
+        for (int i = 2; i <= n; i++) {
+            int temp = a + b;
+            a = b;
+            b = temp;
+        }
+        return b;
     }
     
     /**
